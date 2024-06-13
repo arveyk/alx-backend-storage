e389742a1f6c21c53d3595e9209dcbd0087c3b1e
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-13 21:42:40 +0300

    keyword correction

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- Script to create a users table
-CREATE TABLE IF NOT EXIST users (
+CREATE TABLE IF NOT EXISTS holberton.users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
