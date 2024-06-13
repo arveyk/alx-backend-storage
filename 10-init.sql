d47b29042ad26d90e6b36e5fb79c1c9694228fe7
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-13 16:49:41 +0300

    Advanced MySQL

---
+++ 10-init.sql
@@ -0,0 +1,14 @@
+-- Initial
+DROP TABLE IF EXISTS numbers;
+
+CREATE TABLE IF NOT EXISTS numbers (
+    a int default 0,
+    b int default 0
+);
+
+INSERT INTO students (name, score) VALUES (10, 2);
+INSERT INTO students (name, score) VALUES (4, 5);
+INSERT INTO students (name, score) VALUES (2, 3);
+INSERT INTO students (name, score) VALUES (6, 3);
+INSERT INTO students (name, score) VALUES (7, 0);
+INSERT INTO students (name, score) VALUES (6, 8);
