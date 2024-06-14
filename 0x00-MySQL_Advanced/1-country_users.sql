39c4facb66a8da777d1d31622d40d1d8bb6d9fee
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-14 17:30:28 +0300

    order of constraints

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- Script to create a users table
 CREATE TABLE IF NOT EXISTS users (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255) NOT NULL UNIQUE,
+  email VARCHAR(255) UNIQUE NOT NULL,
   name VARCHAR(255),
   county ENUM('US','CO', 'TN') NOT NULL,
 );
