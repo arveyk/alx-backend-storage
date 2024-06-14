f01d00a2357ef919626e7d5be6c0478725440d6f
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-14 17:12:00 +0300

    hoberton.users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Script to create a users table
 CREATE DATABASE IF NOT EXISTS holberton;
 USE `holberton`;
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS holberton.users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) UNIQUE NOT NULL,
 	name VARCHAR(255)
