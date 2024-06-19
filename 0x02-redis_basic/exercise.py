a74d0b262245cf23b052fe3ab6dbd3a14a5f9cd9
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-19 16:10:27 +0300

    type annotation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,25 +2,30 @@
 """Redis module
 """
 import redis
+from typing import Any
 import uuid
 
 
-class Cache():
+class Cache:
     """Redis Cache Class
     """
-    def __init__(self):
+    def __init__(self) -> None:
         """initializes the instances of Cache class
         """
         _redis = redis.Redis()
         _redis.flushdb()
 
-    def store(self, data: any) -> str:
+    def store(self, data: Any) -> str:
         """
+        Function to store Redis instance
+        Args:
+            Data: data passed to the function
+        Returns: a key string
         """
         red = redis.Redis()
-        # generate random key using uuid
+        """generate random key using uuid"""
         key = str(uuid.uuid4())
-        
+
         # store input_data in redis using the random key
         red.set(key, data)
         return key
