c3f98d3ba49687e4de3deff28819b1cf450dd042
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-19 16:17:30 +0300

    removed annotation from __init__

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,7 +9,7 @@ import uuid
 class Cache:
     """Redis Cache Class
     """
-    def __init__(self) -> None:
+    def __init__(self):
         """initializes the instances of Cache class
         """
         self._redis = redis.Redis()
@@ -17,15 +17,14 @@ class Cache:
 
     def store(self, data: Any) -> str:
         """
-        Function to store Redis instance
+        Function to store Redis instance, generate random key using uuid
+        and store input_data in redis using the random key
         Args:
             Data: data passed to the function
         Returns: a key string
         """
         self._redis = redis.Redis()
-        """generate random key using uuid"""
         key = str(uuid.uuid4())
 
-        # store input_data in redis using the random key
         self._redis.set(key, data)
         return key
